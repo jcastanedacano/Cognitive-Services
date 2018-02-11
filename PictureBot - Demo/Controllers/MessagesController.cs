af743c5490df9294ce6627d4168d7dfab88c124d
Jorge Castañeda <jorge.castaneda@outlook.com>
2018-02-10 19:26:36 -0500

    Add files via upload

---
+++ PictureBot - Demo/Controllers/MessagesController.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace PictureBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Recibir un mensaje de un usuario y responder a él
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implementar eliminación de usuario aquí
+               // Si manejamos la eliminación del usuario, devolvemos un mensaje real
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Controlar los cambios de estado de conversación, como los miembros que se agregan y eliminan
+                // Utilice Activity. MembersAdded y Activity.MembersRemoved y Activity.Action para obtener información
+                // No disponible en todos los canales
+
+        }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing that the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
